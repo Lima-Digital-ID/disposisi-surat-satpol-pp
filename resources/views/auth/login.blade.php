ca987c4d0b4a3e6e1e915b91a3680f3aad658d69
bagusprakasa <47969692+bagusprakasa@users.noreply.github.com>
2022-02-04 12:38:06 +0700

    update favicon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
         content="bootstrap, bootstrap admin template, admin theme, admin dashboard, dashboard template, admin template, responsive" />
     <meta name="author" content="colorlib" />
 
-    <link rel="icon" href="{{ asset('') }}png/jatim.png" type="image/x-icon">
+    <link rel="icon" href="{{ asset('') }}png/satpol-pp.png" type="image/x-icon">
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Quicksand:500,700" rel="stylesheet">
